36fe68bb3c8255aa50d96a97bebd6dca9659dc52
michaelorgarza <michaelorgarza@gmail.com>
2019-01-16 13:15:52 -0600

    update student blade

--- resources/views/batch/student.blade.php
+++ resources/views/batch/student.blade.php
@@ -67,7 +67,6 @@
                     
                     @foreach ($students as $student)
                     <tr>
-                        <td>{{ $student->student_roll }}</td>
                         <td>{{ $student->student_name }}</td>
                         @if($total_classes)
                         <td>{{ round($student->attn/$total_classes*100,2) }} %</td>
